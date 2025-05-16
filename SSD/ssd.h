b1e0dd4499917d7c70079b2e8b6769d0276a00a5
DESKTOP-5FBE16T\User <ruiso@naver.com>
2025-05-16 14:42:27 +0900

    [feat] ssd_init 1

--- SSD/ssd.h
+++ SSD/ssd.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <cstdint>
+
+using namespace std;
 
 class Ssd {
 public:
+	void init(void);
 	void write(int idx, int value);
 	unsigned int read(int idx);
 private:
--- SSD/ssd_test.cpp
+++ SSD/ssd_test.cpp
@@ -4,12 +4,35 @@
 using namespace testing;
 using namespace std;
 
-TEST(SsdTest, write) {
+TEST(SsdTest, init1) {
 	Ssd ssd;
-	ssd.write(0, 0);
+	ssd.init();
 }
 
-TEST(SsdTest, read) {
+TEST(SsdTest, write1) {
+	Ssd ssd;
+	ssd.write(0, 0x123);
+}
+
+TEST(SsdTest, read1) {
 	Ssd ssd;
 	ssd.read(0);
+}
+
+TEST(SsdTest, read2) {
+	Ssd ssd;
+	ssd.write(0, 0x123);
+	EXPECT_EQ(0x123, ssd.read(0));
+}
+
+TEST(SsdTest, read3) {
+	Ssd ssd;
+	ssd.init();
+	EXPECT_EQ(0x123, ssd.read(1));
+}
+
+TEST(SsdTest, read4) {
+	Ssd ssd;
+	ssd.init();
+	EXPECT_EQ(0x12345678, ssd.read(2));
 }
