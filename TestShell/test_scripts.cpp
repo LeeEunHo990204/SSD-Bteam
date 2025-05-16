36327680dbad356b7ac3a3ac14f1a9b7355f5278
donghwa.kang <dh.kang8828@gmail.com>
2025-05-16 14:51:56 +0900

    [feat] implement TC1 : creating TestScripts1 instance

--- TestShell/test_scripts.cpp
+++ TestShell/test_scripts.cpp
@@ -1,2 +1,28 @@
+#include "gmock/gmock.h"
 #include "TestScripts.h"
 
+class TescScriptsFixture : public ::testing::Test
+{
+public:
+	ITestScripts* testScripts;
+	void factoryCreateTS1()
+	{
+		testScripts = new TestScripts1();
+	}
+	
+	void deleteTS() {
+		if (testScripts != nullptr)
+			delete testScripts;
+	}
+
+	std::string EMPTY_SCRIPT_NAME = "empty_script";
+};
+
+TEST_F(TescScriptsFixture, TestCreateTS1instance)
+{
+	//Arrange, ACT
+	factoryCreateTS1();
+
+	//Assert
+	EXPECT_NE(testScripts, nullptr);
+}
