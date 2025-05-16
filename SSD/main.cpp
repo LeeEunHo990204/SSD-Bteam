8ab155ecd498b4d503c2fcd0dc212d7a9ce9112b
dongju.kim <djk70711@naver.com>
2025-05-16 14:27:21 +0900

    feat : 코드리뷰 반영하여 data 유효성 검사 추가, ssd class 함수 호출부 출가

--- SSD/main.cpp
+++ SSD/main.cpp
@@ -1,7 +1,18 @@
 #include "gmock/gmock.h"
+#include "ssd.h"
 
 using namespace testing;
 using namespace std;
+
+bool isOutOf4ByteRange(const std::string& hexStr) {
+    try {
+        unsigned long long value = std::stoull(hexStr, nullptr, 16);
+        return value > 0xFFFFFFFFULL;
+    }
+    catch (const std::exception&) {
+        return true;
+    }
+}
 
 int main(int argc, char* argv[]) {
 #if defined(_DEBUG)
@@ -18,26 +29,37 @@ int main(int argc, char* argv[]) {
     string cmd = argv[1];
     int address = stoi(argv[2]);
 
+    if (address < 0 || address > 99)
+    {
+        cerr << "LBA Out of Range" << endl;
+        return 1;
+    }
+    Ssd ssd;
+    
     cout << "CMD :" << cmd << endl;
     cout << "argc :" << argc << endl;
     if (cmd == "R") {
-        //readData(address);
-        cout << "read~" << endl;
+        ssd.read(address);
     }
     else if (cmd == "W") {
         if (argc < 4) {
             cerr << "ERROR" << endl;
             return 1;
         }
-        cout << "write~" << endl;
+        if (isOutOf4ByteRange(argv[3])) {
+            std::cout << "Out of 4-byte range!" << std::endl;
+            return 1;
+        }
 
-        //writeData(address, data);
+        unsigned int data = std::stoul(argv[3], nullptr, 16);
+        ssd.write(address, data);
         
     }
     else {
         cerr << "Unknown command: " << cmd << endl;
         return 1;
     }
+
 	return 0;
 #endif
 }
