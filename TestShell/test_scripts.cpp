3938730c0f3c68aa2b70901e333babc76ec3920b
DESKTOP-5FBE16T\User <dh.kang8828@gmail.com>
2025-05-19 09:57:20 +0900

    [feat] add Test for creating TS instance

--- TestShell/test_scripts.cpp
+++ TestShell/test_scripts.cpp
@@ -5,9 +5,9 @@ class TescScriptsFixture : public ::testing::Test
 {
 public:
 	ITestScripts* testScripts;
-	void factoryCreateTS1()
+	void factoryCreateTS(std::string tsName)
 	{
-		testScripts = new TestScripts1();
+		testScripts = new TestScripts1(tsName);
 	}
 	
 	void deleteTS() {
@@ -16,14 +16,41 @@ public:
 	}
 
 	std::string EMPTY_SCRIPT_NAME = "empty_script";
+	std::string TS1_NAME = "1_FullWriteAndReadCompare";
+	std::string TS2_NAME = "2_PartialLBAWrite";
+	std::string TS3_NAME = "3_PartialLBARead";
 };
 
 TEST_F(TescScriptsFixture, TestCreateTS1instance)
 {
-	//Arrange, ACT
-	//factoryCreateTS1();
+	//Arrange
+	factoryCreateTS(TS1_NAME);
 
-	//Assert
-	//EXPECT_NE(testScripts, nullptr);
+	//ACT, Assert
+	EXPECT_EQ(testScripts->getName(), TS1_NAME);
+
+	deleteTS();
+}
+
+TEST_F(TescScriptsFixture, TestCreateTS2instance)
+{
+	//Arrange
+	factoryCreateTS(TS2_NAME);
+
+	//ACT, Assert
+	EXPECT_EQ(testScripts->getName(), TS2_NAME);
+
+	deleteTS();
+}
+
+TEST_F(TescScriptsFixture, TestCreateTS3instance)
+{
+	//Arrange
+	factoryCreateTS(TS3_NAME);
+
+	//ACT, Assert
+	EXPECT_EQ(testScripts->getName(), TS3_NAME);
+
+	deleteTS();
 }
 
