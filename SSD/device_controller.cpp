76a36c5ea2da78f479345419d4b1cbd3b0f37948
dongju.kim <djk70711@naver.com>
2025-05-19 16:05:23 +0900

    fix : 불필요한 로깅 제거

--- SSD/device_controller.cpp
+++ SSD/device_controller.cpp
@@ -3,16 +3,15 @@
 DeviceController::DeviceController(IDevice* device) : device_(device) { ; }
 
 int DeviceController::run(const CommandInfo& commandInfo) {
-    if (commandInfo.command == "W") {
-        device_->write(commandInfo.address, commandInfo.data);
-    }
-    else if (commandInfo.command == "R") {
-        unsigned int val = device_->read(commandInfo.address);
-        std::cout << "Read: 0x" << std::hex << val << "\n";
-    }
-    else {
-        std::cerr << "Invalid command\n";
-        return 1;
-    }
+	if (commandInfo.command == "R") {
+		device_->read(commandInfo.address);
+	}
+	else if (commandInfo.command == "W") {
+		device_->write(commandInfo.address, commandInfo.data);
+	}
+	else {
+		cerr << "Unknown command: " << commandInfo.command << endl;
+		return 1;
+	}
     return 0;
 }
