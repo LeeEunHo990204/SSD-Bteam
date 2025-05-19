64c3b84f72d8878337a1df44f26f95158f07725f
Donggeon-kim <rjsdl2210@naver.com>
2025-05-19 09:16:24 +0900

    [refactor] Change test name & Add Test Case

--- SSD/ssd_test.cpp
+++ SSD/ssd_test.cpp
@@ -8,31 +8,43 @@ TEST(SsdTest, constructor) {
 	SSD ssd;
 }
 
-TEST(SsdTest, write1) {
+TEST(SsdTest, writeTrue) {
 	SSD ssd;
 	ssd.write(0, 0x123);
 	ssd.write(60, 0x456);
 }
 
-TEST(SsdTest, read1) {
+TEST(SsdTest, writeFalse) {
+	SSD ssd;
+	ssd.write(0, 0x123);
+}
+
+TEST(SsdTest, readTrue) {
 	SSD ssd;
 	ssd.read(0);
 }
 
-TEST(SsdTest, read2) {
+TEST(SsdTest, readFalse) {
+	SSD ssd;
+	ssd.read(100);
+}
+
+TEST(SsdTest, readWriteTrue) {
 	SSD ssd;
 	ssd.write(0, 0x123);
 	EXPECT_EQ(0x123, ssd.read(0));
 }
 
-TEST(SsdTest, read3) {
+TEST(SsdTest, readWriteFalse) {
 	SSD ssd;
 	ssd.write(1, 0x123);
-	EXPECT_EQ(0x123, ssd.read(1));
+	EXPECT_NE(0x456, ssd.read(1));
 }
 
-TEST(SsdTest, read4) {
+TEST(SsdTest, writeDuplicateTrue) {
 	SSD ssd;
-	ssd.write(2, 0x12345678);
-	EXPECT_EQ(0x12345678, ssd.read(2));
+	ssd.write(0, 0x12345678);
+	ssd.write(50, 0x5678);
+	ssd.write(0, 0x5678);
+	EXPECT_EQ(0x5678, ssd.read(0));
 }
