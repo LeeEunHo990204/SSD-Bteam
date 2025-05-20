ddca16de97a3243b363f1830a8855c1fad4c82a5
DESKTOP-5FBE16T\User <nevernever>
2025-05-20 13:51:52 +0900

    [Feature] Add getLogfileSize function to save log

--- TestShell/Logger.cpp
+++ TestShell/Logger.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <sys/stat.h>
+#include <thread>
+#include <ctime>
 #include <string>
+
+using namespace std;
 
 class Logger {
 public:
@@ -6,10 +14,31 @@ public:
 
 	}
 
-	void saveLog() {
+    void saveLog() {
+        //mFile.open(mLogName);
+        //for (int i = 0; i < (1024 * 3); i++) {
+        //    mDataBuffer << "1";
+        //}
+        //mFile << mDataBuffer.str();
 
-	}
+        int file_size = getLogfileSize();
+        //cout << endl << "#############" << endl << mLogName << ":" << file_size << endl << "#############" << endl;
+        if (file_size >= (1024 * 3)) {
+            mFile.close();
+            rename(mLogName.c_str(), mNewLogName.c_str());
+            mFile.open(mLogName);
+        }
+    }
 
 private:
-
+    time_t mTimer = time(NULL);
+    ostringstream mDataBuffer;
+    ofstream mFile;
+    const string mLogName = "latest.log";
+    string mNewLogName = "timecode_TODO.log";
+    
+    int getLogfileSize()
+    {
+        return mFile.tellp();
+    }
 };
