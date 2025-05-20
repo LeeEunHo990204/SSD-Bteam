a4df21be162c2cfa02b2cb0df5e961d975c6670f
eunho.lee <eno99.lee@samsung.com>
2025-05-20 15:23:07 +0900

    [feat] Unit Test 추가

--- TestShell/test_logger.cpp
+++ TestShell/test_logger.cpp
@@ -15,7 +15,21 @@ public:
 	MockLauncher mockLauncher;
 };
 
-TEST_F(LoggerFixture, TestPrintLog)
+TEST_F(LoggerFixture, DISABLED_TestPrintLog)
+{
+	logger = new Logger();
+	logger->print("test", "test");
+}
+
+TEST_F(LoggerFixture, DISABLED_TestPrintLog2Files)
+{
+	logger = new Logger();
+	for (int i = 0; i < 200; i++) {
+		logger->print("test" + to_string(i), "test");
+	}
+}
+
+TEST_F(LoggerFixture, TestPrintLogOver3Files)
 {
 	logger = new Logger();
 	for (int i = 0; i < 1000; i++) {
