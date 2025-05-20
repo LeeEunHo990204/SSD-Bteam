3552d28f357a875c38f87f2b64750894dd1b1b18
eunho.lee <eno99.lee@samsung.com>
2025-05-20 13:15:10 +0900

    [feat] getTimeString, LoggerTest 구현

--- TestShell/Logger.cpp
+++ TestShell/Logger.cpp
@@ -1,8 +1,14 @@
+#pragma once
+#include <iostream>
 #include <string>
+#include <iomanip>
+#include <ctime>
+#include <sstream>
 
 class Logger {
 public:
 	void print(std::string className, std::string message) {
+		std::string timeString = getTimeString();
 
 	}
 
@@ -11,5 +17,13 @@ public:
 	}
 
 private:
+	std::string getTimeString() {
+		std::time_t now = std::time(nullptr);
+		std::tm local_tm;
+		localtime_s(&local_tm, &now);
 
+		std::ostringstream oss;
+		oss << std::string("[") << std::put_time(&local_tm, "%y.%m.%d %H:%M") << "]";
+		return oss.str();
+	}
 };
