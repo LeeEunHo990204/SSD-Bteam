d15bcc8a1d0c0a95ac7992893f185a02e7c8bf7f
eunho.lee <eno99.lee@samsung.com>
2025-05-21 14:59:29 +0900

    [refactor] Icmd로 command별 execute 다형성 추가

--- TestShell/Shell.h
+++ TestShell/Shell.h
@@ -97,10 +97,113 @@ private:
 	bool isValid;
 };
 
-class Shell {
+class ICmd {
 public:
-	Shell(ICmdLauncher* cmdLauncher, ITestScripts* testScripts) :
-		cmdLauncher(cmdLauncher), testScripts(testScripts) {
+	virtual ~ICmd() {}
+	virtual std::string execute(ICmdLauncher* cmdLauncher, Command* command) = 0;
+};
+
+class Write : public ICmd {
+public:
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		int LBA = stoi(command->params[0]);
+		if (isOutOf4ByteRange(command->params[1])) {
+			return "Out of 4-byte range!";
+		}
+		unsigned int val = stoul(command->params[1], nullptr, 16);
+		cmdLauncher->write(LBA, val);
+		return "[Write] Done";
+	}
+};
+
+class Read : public ICmd {
+public:
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		int LBA = stoi(command->params[0]);
+		if (LBA >= STORAGE_SIZE || LBA < 0)
+			return std::string("[Read] LBA ") + std::to_string(LBA) + std::string(" : ") + std::string("ERROR");
+		return std::string("[Read] LBA ") + std::to_string(LBA) + std::string(" : ") + cmdLauncher->read(LBA);
+	}
+};
+
+class Erase : public ICmd {
+public:
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		int LBA = stoi(command->params[0]);
+		int size = stoi(command->params[1]);
+		int startLBA = 0;
+		int endLBA = 0;
+		if (LBA >= STORAGE_SIZE || LBA < 0) {
+			cmdLauncher->erase(LBA, size);
+			return "[Erase] ERROR";
+		}
+		startLBA = LBA;
+		endLBA = LBA + size - 1;
+		if (size < 0) {
+			startLBA = LBA + size + 1;
+			size *= -1;
+			if (startLBA < 0) startLBA = 0;
+			endLBA = LBA;
+		}
+		if (endLBA >= STORAGE_SIZE) endLBA = 99;
+		for (int i = startLBA; i <= endLBA; i += 10) {
+			if (endLBA - i + 1 < 10) {
+				if (!cmdLauncher->erase(i, endLBA - i + 1)) "[Erase] ERROR";
+			}
+			else {
+				if (!cmdLauncher->erase(i, 10)) "[Erase] ERROR";
+			}
+		}
+		return "[Erase] Done";
+	}
+};
+
+class EraseRange : public ICmd {
+public:
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		int startLBA = stoi(command->params[0]);
+		int endLBA = stoi(command->params[1]);
+
+		if (startLBA >= STORAGE_SIZE || startLBA < 0 || endLBA >= STORAGE_SIZE || endLBA < 0) {
+			cmdLauncher->erase(-1, -1);
+			return "[Erase_range] ERROR";
+		}
+
+		if (startLBA > endLBA) {
+			swap(startLBA, endLBA);
+		}
+		for (int i = startLBA; i <= endLBA; i += 10) {
+			if (endLBA - i + 1 < 10) {
+				if (!cmdLauncher->erase(i, endLBA - i + 1)) "[Erase_range] ERROR";
+			}
+			else {
+				if (!cmdLauncher->erase(i, 10)) "[Erase_range] ERROR";
+			}
+		}
+
+		return "[Erase_range] Done";
+	}
+};
+
+class Flush : public ICmd {
+public:
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		cmdLauncher->flush();
+		return "[Flush] Done";
+	}
+};
+
+class Exit : public ICmd {
+public:
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		Logger::getInstance().saveLog(true);
+		return "EXIT";
+	}
+};
+
+class Help : public ICmd {
+public:
+	Help() {
 		mHelp.insert(make_pair("write (or WRITE)", "write 3 0xAAAABBBB -> write 3rd LBA with value 0xAAAABBBB"));
 		mHelp.insert(make_pair("read (or READ)", "read 3 -> read 3rd LBA"));
 		mHelp.insert(make_pair("erase (or ERASE)", "erase 3 5 -> erase 3~7 LBA(size:5)"));
@@ -113,6 +216,89 @@ public:
 		mHelp.insert(make_pair("4_ (or 4_EraseAndWriteAging)", "TestScript4: Write and Overwrite and Erase LBA with it's scenario"));
 		mHelp.insert(make_pair("exit (or EXIT)", "Exit testShell"));
 		mHelp.insert(make_pair("help (or HELP)", "Print team name and members then explain all commands"));
+	}
+
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		printHelp();
+		return "HELP";
+	}
+
+private:
+	unordered_map<string, string>  mHelp;
+	void printHelp() {
+		cout << "################ TEAM : Best Reviewer : KDH, KSI, KDG, KDJ, PYH, LEH ################" << endl << endl;
+		cout << std::left << std::setw(40) << ">> Command" << " : " << ">> Description" << endl;
+		for (auto it = mHelp.begin(); it != mHelp.end(); it++) {
+			cout << std::left << std::setw(40) << it->first << " : " << it->second << endl;
+		}
+	}
+};
+
+class Fullwrite : public ICmd {
+public:
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		if (isOutOf4ByteRange(command->params[0])) {
+			return "Out of 4-byte range!";
+		}
+		unsigned int val = stoul(command->params[0], nullptr, 16);
+		for (int LBA = 0; LBA < STORAGE_SIZE; LBA++) {
+			cmdLauncher->write(LBA, val);
+		}
+		return "[FullWrite] Done";
+	}
+};
+
+class Fullread : public ICmd {
+public:
+	std::string execute(ICmdLauncher* cmdLauncher, Command* command) override {
+		std::string result = "[FullRead]";
+		for (int LBA = 0; LBA < 100; LBA++) {
+			result += std::string("LBA ") + std::to_string(LBA) + std::string(" : ") + cmdLauncher->read(LBA);
+			if (LBA < 99) result += std::string("\n");
+		}
+		return result;
+	}
+};
+
+class CmdFactory {
+public:
+	static std::unique_ptr<ICmd> create(const std::string& cmdType) {
+		if (cmdType == "write" || cmdType == "WRITE") {
+			return std::make_unique<Write>();
+		}
+		if (cmdType == "read" || cmdType == "READ") {
+			return std::make_unique<Read>();
+		}
+		if (cmdType == "erase" || cmdType == "ERASE") {
+			return std::make_unique<Erase>();
+		}
+		if (cmdType == "erase_range" || cmdType == "ERASE_RANGE") {
+			return std::make_unique<EraseRange>();
+		}
+		if (cmdType == "flush" || cmdType == "FLUSH") {
+			return std::make_unique<Flush>();
+		}
+		if (cmdType == "exit" || cmdType == "EXIT") {
+			return std::make_unique<Exit>();
+		}
+		if (cmdType == "help" || cmdType == "HELP") {
+			return std::make_unique<Help>();
+		}
+		if (cmdType == "fullwrite" || cmdType == "FULLWRITE") {
+			return std::make_unique<Fullwrite>();
+		}
+		if (cmdType == "fullread" || cmdType == "FULLREAD") {
+			return std::make_unique<Fullread>();
+		}
+		// 기타 명령 추가
+		return nullptr;
+	}
+};
+
+class Shell {
+public:
+	Shell(ICmdLauncher* cmdLauncher, ITestScripts* testScripts) :
+		cmdLauncher(cmdLauncher), testScripts(testScripts) {
 	}
 
 	Shell(ICmdLauncher* cmdLauncher) : cmdLauncher(cmdLauncher) {
@@ -139,112 +325,10 @@ public:
 			return "INVALID COMMAND";
 		}
 
-		if (command->cmd == "write" || command->cmd == "WRITE") {
-			int LBA = stoi(command->params[0]);
-			if (isOutOf4ByteRange(command->params[1])) {
-				return "Out of 4-byte range!";
-			}
-			unsigned int val = stoul(command->params[1], nullptr, 16);
-			cmdLauncher->write(LBA, val);
-			return "[Write] Done";
-		}
+		auto cmd = CmdFactory::create(command->cmd);
+		return cmd->execute(cmdLauncher, command);
 
-		else if (command->cmd == "read" || command->cmd == "READ") {
-			int LBA = stoi(command->params[0]);
-			if (LBA >= STORAGE_SIZE || LBA < 0)
-				return std::string("[Read] LBA ") + std::to_string(LBA) + std::string(" : ") + std::string("ERROR");
-			return std::string("[Read] LBA ") + std::to_string(LBA) + std::string(" : ") + cmdLauncher->read(LBA);
-		}
-
-		else if (command->cmd == "erase" || command->cmd == "ERASE") {
-			int LBA = stoi(command->params[0]);
-			int size = stoi(command->params[1]);
-			int startLBA = 0;
-			int endLBA = 0;
-			if (LBA >= STORAGE_SIZE || LBA < 0) {
-				cmdLauncher->erase(LBA, size);
-				return "[Erase] ERROR";
-			}
-			startLBA = LBA;
-			endLBA = LBA + size - 1;
-			if (size < 0) {
-				startLBA = LBA + size + 1;
-				size *= -1;
-				if (startLBA < 0) startLBA = 0;
-				endLBA = LBA;
-			}
-			if (endLBA >= STORAGE_SIZE) endLBA = 99;
-			for (int i = startLBA; i <= endLBA; i += 10) {
-				if (endLBA - i + 1 < 10) {
-					if (!cmdLauncher->erase(i, endLBA - i + 1)) "[Erase] ERROR";
-				}
-				else {
-					if (!cmdLauncher->erase(i, 10)) "[Erase] ERROR";
-				}
-			}
-			return "[Erase] Done";
-		}
-
-		else if (command->cmd == "erase_range" || command->cmd == "ERASE_RANGE") {
-			int startLBA = stoi(command->params[0]);
-			int endLBA = stoi(command->params[1]);
-
-			if (startLBA >= STORAGE_SIZE || startLBA < 0 || endLBA >= STORAGE_SIZE || endLBA < 0) {
-				cmdLauncher->erase(-1, -1);
-				return "[Erase_range] ERROR";
-			}
-
-			if (startLBA > endLBA) {
-				swap(startLBA, endLBA);
-			}
-			for (int i = startLBA; i <= endLBA; i += 10) {
-				if (endLBA - i + 1 < 10) {
-					if (!cmdLauncher->erase(i, endLBA - i + 1)) "[Erase_range] ERROR";
-				}
-				else {
-					if (!cmdLauncher->erase(i, 10)) "[Erase_range] ERROR";
-				}
-			}
-
-			return "[Erase_range] Done";
-		}
-
-		else if (command->cmd == "flush" || command->cmd == "FLUSH") {
-			cmdLauncher->flush();
-			return "[Flush] Done";
-		}
-
-		else if (command->cmd == "exit" || command->cmd == "EXIT") {
-			Logger::getInstance().saveLog(true);
-			return "EXIT";
-		}
-
-		else if (command->cmd == "help" || command->cmd == "HELP") {
-			printHelp();
-			return "HELP";
-		}
-
-		else if (command->cmd == "fullwrite" || command->cmd == "FULLWRITE") {
-			if (isOutOf4ByteRange(command->params[0])) {
-				return "Out of 4-byte range!";
-			}
-			unsigned int val = stoul(command->params[0], nullptr, 16);
-			for (int LBA = 0; LBA < STORAGE_SIZE; LBA++) {
-				cmdLauncher->write(LBA, val);
-			}
-			return "[FullWrite] Done";
-		}
-
-		else if (command->cmd == "fullread" || command->cmd == "FULLREAD") {
-			std::string result = "[FullRead]";
-			for (int LBA = 0; LBA < 100; LBA++) {
-				result += std::string("LBA ") + std::to_string(LBA) + std::string(" : ") + cmdLauncher->read(LBA);
-				if (LBA < 99) result += std::string("\n");
-			}
-			return result;
-		}
-
-		else if (command->cmd == "1_" || command->cmd == "1_FullWriteAndReadCompare") {
+		if (command->cmd == "1_" || command->cmd == "1_FullWriteAndReadCompare") {
 			setTestScripts(new TestScripts1("1_FullWriteAndReadCompare", cmdLauncher));
 			testScripts->runTestScenario();
 			if (testScripts->getResult() == 0) return "PASS";
@@ -280,15 +364,6 @@ public:
 private:
 	ICmdLauncher* cmdLauncher;
 	ITestScripts* testScripts;
-	unordered_map<string, string>  mHelp;
-
-	void printHelp() {
-		cout << "################ TEAM : Best Reviewer : KDH, KSI, KDG, KDJ, PYH, LEH ################" << endl << endl;
-		cout << std::left << std::setw(40) << ">> Command" << " : " << ">> Description" << endl;
-		for (auto it = mHelp.begin(); it != mHelp.end(); it++) {
-			cout << std::left << std::setw(40) << it->first << " : " << it->second << endl;
-		}
-	}
 
 	std::vector<std::string> splitBySpace(const std::string& input) {
 		std::istringstream iss(input);
